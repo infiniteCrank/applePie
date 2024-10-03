010f5ba71adc19c6ebb92b664adb0d0154f0b2aa
Julian Wilkison-Duran <clockworksyler@gmail.com>
2024-10-03 03:52:11 -0400

    comment out promise example for now

--- main.js
+++ main.js
@@ -3,18 +3,17 @@ window.addEventListener("load", (event) => {
     var mainHeader = document.createElement("h1")
     mainElement.appendChild(mainHeader)
 
-    getJQuery()
-    Promise.all([getJQuery(),Promise.resolve(3)]).then(values => {
-        console.log(values)
-        mainHeader.innerHTML = values[0]
-        mainElement.innerHTML += values[1]
-    })
+    // Promise.all([getJQuery(),Promise.resolve(3)]).then(values => {
+    //     console.log(values)
+    //     mainHeader.innerHTML = values[0]
+    //     mainElement.innerHTML += values[1]
+    // })
 
     
 });
 
-function getJQuery() {
-    return fetch("https://code.jquery.com/jquery-2.2.4.min.js")
-    .then(x => x.text())
-    .then(x => x.substring(17,10))
-}
+// function getJQuery() {
+//     return fetch("https://code.jquery.com/jquery-2.2.4.min.js")
+//     .then(x => x.text())
+//     .then(x => x.substring(17,10))
+// }
