f4c491ee5cad894b1cd3c36def1c235fa3362e1f
Julian Wilkison-Duran <clockworksyler@gmail.com>
2024-10-03 03:22:18 -0400

    basic promise setup complete

--- main.js
+++ main.js
@@ -3,5 +3,16 @@ window.addEventListener("load", (event) => {
     var mainHeader = document.createElement("h1")
     mainHeader.innerHTML = "Welcome to Apple Pie!"
     mainElement.appendChild(mainHeader)
+
+    getJQuery()
+    Promise.all([getJQuery()]).then(values => {
+        console.log(values)
+    })
     
 });
+
+function getJQuery() {
+    return fetch("https://code.jquery.com/jquery-2.2.4.min.js")
+    .then(x => x.text())
+    .then(x => x.substring(17,10))
+}
